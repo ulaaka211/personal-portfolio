5c83d75aeeb166db6340ca08263f3f23fc740e5e
ulaaka211 <uulaaka73gmail.com>
2024-05-28 23:03:33 +0800

    kk

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,7 @@ import { Introduction } from "./_component/Introduction";
 import { Works } from "./_component/Works";
 import { Header } from "./_component/Header";
 import { Contact } from "./_component/Contact";
+import { Footer } from "./_component/Footer";
 
 export default function Home() {
   return (
