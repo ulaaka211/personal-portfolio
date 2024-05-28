01601a2c45bb59566739e6580aace22300e8671b
ulaaka211 <uulaaka73gmail.com>
2024-05-28 23:02:49 +0800

    kk

--- src/app/_component/Footer.jsx
+++ src/app/_component/Footer.jsx
@@ -1,7 +1,4 @@
 export const Footer = () => {
-  const email = "uulaaka73@gmail.com";
-  const phone = "80551671";
-
   return (
     <footer className="md:w-full  flex items-center justify-center px-8 py-8 bg-[#2D2E32]">
       <p className="text-lg text-white text-center">
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,6 @@ import { AboutMe } from "./_component/AboutMe";
 import { Introduction } from "./_component/Introduction";
 import { Works } from "./_component/Works";
 import { Header } from "./_component/Header";
-import { Footer } from "./_component/footer";
 import { Contact } from "./_component/Contact";
 
 export default function Home() {
