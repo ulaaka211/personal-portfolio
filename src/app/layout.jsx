dd9c453cca0bae930477a6b648c2c5a86a230e76
ulaaka211 <uulaaka73gmail.com>
2024-06-03 20:20:57 +0800

    kk

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -14,7 +14,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body
+        className={inter.className}
+        style={{
+          backgroundColor: "white",
+        }}
+      >
         <ThemeProvider>
           {children}
 
