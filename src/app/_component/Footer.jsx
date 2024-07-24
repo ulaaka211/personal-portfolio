a836d83a92878e0e1597a6f2cae60063d8195b67
ulaaka211 <uulaaka73gmail.com>
2024-07-24 17:00:04 +0800

    kk

--- src/app/_component/Footer.jsx
+++ src/app/_component/Footer.jsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <footer className="md:w-full flex items-center justify-center px-8 py-8 bg-[#2D2E32]">
       <p className="text-lg text-white text-center">
-        © 2024 | S.Saikhanbayar. All rights reserved.
+        © 2024 | S Saikhanbayar. All rights reserved.
       </p>
     </footer>
   );
